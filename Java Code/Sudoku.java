c791891da8a49c308751b7ad57b6785a84a30db2
Atish Adhav <adhavatish04@gmail.com>
2025-03-07 06:13:12 +0530

    second change

---
+++ Java Code/Sudoku.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Sudoku {
+
+    public static boolean isSafe(int sudoku[][], int row, int col, int digit) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][col] == digit || sudoku[row][i] == digit) {
+                return false;
+            }
+        }
+        int sr = (row / 3) * 3, sc = (col / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean sudokuSolver(int sudoku[][], int row, int col) {
+        if (row == 9) return true;
+        int nextRow = (col == 8) ? row + 1 : row;
+        int nextCol = (col + 1) % 9;
+
+        if (sudoku[row][col] != 0) {
+            return sudokuSolver(sudoku, nextRow, nextCol);
+        }
+
+        for (int digit = 1; digit <= 9; digit++) {
+            if (isSafe(sudoku, row, col, digit)) {
+                sudoku[row][col] = digit;
+                if (sudokuSolver(sudoku, nextRow, nextCol)) return true;
+                sudoku[row][col] = 0; // Backtrack
+            }
+        }
+        return false;
+    }
+
+    public static void printSudoku(int sudoku[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sudoku[][] = new int[9][9];
+
+        System.out.println("Enter the Sudoku grid (use 0 for empty cells):");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sudoku[i][j] = sc.nextInt();
+              
+            }
+        }
+
+        System.out.println();
+
+        if (sudokuSolver(sudoku, 0, 0)) {
+            System.out.println("Solution exists:");
+            printSudoku(sudoku);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+}
